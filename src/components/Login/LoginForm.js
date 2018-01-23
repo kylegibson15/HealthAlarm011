86b02e9ea6b2065e4fe190826106307448d0e3f7
kylegibson15 <kylegibson15@gmail.com>
2018-01-22 22:56:59 -0700

    form styled, not connected to database. issue passing data

---
+++ src/components/Login/LoginForm.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+
+export default class LoginForm extends Component {
+
+  handleLoginEvent = (event) => {
+    event.preventDefault();
+    // const usernameInputValue = this.refs.usernameInput.value;
+    // const passwordInputValue = this.refs.passwordInput.value;
+    const usernameInputValue = 'kyle';
+    const passwordInputValue = 'gibson'
+    this.props.handleLogin(usernameInputValue, passwordInputValue);
+  }
+
+  render() {
+    return (<View style={styles.container}>
+      <StatusBar
+      barStyle="dark-content"
+    />
+      <TextInput
+        placeholder="username or email"
+        placeholderTextColor="white"
+        returnKeyType="next"
+        ref={(input) => this.usernameInput = input}
+        onChangeText={(username) => this.usernameInputValue = username}
+        onSubmitEditing={() => this.passwordInput.focus()}
+        keyboardType="email-address"
+        autoCapitolize={false}
+        style={styles.input}
+      />
+
+      <TextInput
+        secureTextEntry
+        placeholder="password"
+        placeholderTextColor="white"
+        returnKeyType="go"
+        ref={(input) => this.passwordInput = input}
+        autoCapitolize={false}
+        style={styles.input}
+      />
+
+      <TouchableOpacity onPress={this.handleLoginEvent} style={styles.buttonContiainer}>
+        <Text style={styles.buttonText}>
+          Login
+        </Text>
+      </TouchableOpacity>
+    </View>)
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: '#f39c12',
+  },
+  input: {
+    height: 40,
+    backgroundColor: 'rgba(255,255,255, 0.2)',
+    marginBottom: 10,
+    color: '#FFF',
+    paddingHorizontal: 10,
+  },
+  buttonContiainer:{
+    backgroundColor: '#2c3e50',
+    paddingVertical: 15
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontWeight: '700',
+  }
+})
